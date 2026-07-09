96ab126cdfeb467b087f8f21ba24712bc6652287
ajit <azit.ghising@gmail.com>
2026-07-09 23:35:37 +0930

    This is the initial task manager commit.

---
+++ src/models.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Task {
+    id: String,
+    title: String,
+    description: String,
+    priority: Priority,
+    due_date: Option<String>,
+    status: Status,
+    created_at: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+enum Priority{
+    High,
+    Medium,
+    Low
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+enum Status {
+    Pending,
+    Completed
+}
+
+fn main() {
+    println!("Hello, world!");
+}
