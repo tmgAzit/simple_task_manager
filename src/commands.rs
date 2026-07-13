6a01d4993af0c69ac7c4ae4ad37e99f3a8952760
ajit <azit.ghising@gmail.com>
2026-07-14 02:00:48 +0930

    Add function for more robust.

--- src/commands.rs
+++ src/commands.rs
@@ -1,4 +1,4 @@
-use crate::models::{Status, Task};
+use crate::models::{Status, Task, Priority};
 use crate::storage::{load_tasks, save_tasks};
 use std::env;
 use std::io::{self, Write};
@@ -11,7 +11,7 @@ pub fn run() {
         println!("Command: add, list, done [id], delete [id]");
         return;
     }
-    match args[1].as_str() {
+    match args[1].to_lowercase().as_str() {
         "add" => add_task(),
         "list" => list_tasks(),
         "done" => complete_task(&args),
@@ -21,14 +21,49 @@ pub fn run() {
 }
 
 pub fn add_task() {
+   // Title section
     println!("Enter the title: ");
     io::stdout().flush().unwrap();
 
     let mut title = String::new();
     io::stdin().read_line(&mut title).unwrap();
     let title = title.trim().to_string();
+    
+    if title.is_empty(){
+        println!("Title cannot be empty!");
+        return;
+    };
 
-    let task = Task::new(title);
+    //Priority section
+    println!("Priority of the task (high/medium/low): ");
+    io::stdout().flush().unwrap();
+
+    let mut priority_value = String::new();
+    io::stdin().read_line(&mut priority_value).unwrap();
+
+    let priority = match priority_value.trim().to_lowercase().as_str() {
+        "high" => Priority::High,
+        "medium" => Priority::Medium,
+        "low" => Priority::Low,
+        _ => {println!("Invalid Priority! Defaulting to Medium");
+            Priority::Medium}
+    };
+    
+    // Due date
+    println!("Due date: YYYY-MM-DD or skip pressing Enter: ");
+    io::stdout().flush().unwrap();
+
+    let mut due_date = String::new();
+    io::stdin().read_line(&mut due_date).unwrap();
+
+    let due_date = if due_date.trim().is_empty() {
+        None
+    } else {
+        Some(due_date.to_string())
+    };
+
+    let task = Task::new(title, priority, due_date);
+
 
     let mut tasks = load_tasks();
     tasks.push(task);
@@ -83,7 +118,7 @@ pub fn delete_task(args: &[String]) {
 
     if tasks.len() < initial_len {
         save_tasks(&tasks);
-        println!("Sucessfully new task with the title is added");
+        println!("Sucessfully Deleted!!!");
     } else {
         println!("The task with {} is not found!", id);
     }
